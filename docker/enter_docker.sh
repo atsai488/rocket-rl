25bf4a625c295c2e1aaf45147fd4eabd48339de8
Austin Tsai <aytsai@usc.edu>
2026-04-08 21:21:14 -0700

    lazy fr

--- docker/enter_docker.sh
+++ docker/enter_docker.sh
@@ -4,14 +4,12 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 REPO_ROOT="$(cd "${SCRIPT_DIR}/.." && pwd)"
 
 docker run -it \
-	--entrypoint /bin/bash \
 	--privileged \
 	--network host \
 	--device /dev/i2c-1 \
+	--workdir /rocket/jetson \
 	-v "${REPO_ROOT}/jetson:/rocket/jetson" \
 	-v /proc//device-tree:/proc/device-tree:ro \
 	-v /sys:/sys:ro \
-	rocket
-
-cd jetson/
-python3 rocket_rl.py rocket_direct.onnx
+	rocket \
+	python3 rocket_rl.py rocket_direct.onnx
