90355d8930482550f841499dfee64613227a904b
Austin Tsai <aytsai@usc.edu>
2026-04-16 13:36:26 -0700

    a

--- testfiles/encoder_test.py
+++ testfiles/encoder_test.py
@@ -140,10 +140,10 @@ def main():
             while True:
                 # enable_status = send_and_read_status(ser, build_enable_command(addr=target_addr, enable=True))
                 move_status = send_and_read_status(ser, build_move_command(addr=target_addr, speed=10, direction=0))
-                time.sleep(0.1)
+                time.sleep(0.02)
                 move_status = send_and_read_status(ser, build_move_command(addr=target_addr, speed=10, direction=1))
                 # print(f"Servo {target_addr}: enabled (status=0x{enable_status:02X}), move command sent at speed=1 (status=0x{move_status:02X})")
-                time.sleep(0.1)
+                time.sleep(0.02)
             move_status = send_and_read_status(ser, build_stop_command(addr=target_addr))
 
         except Exception as e:
